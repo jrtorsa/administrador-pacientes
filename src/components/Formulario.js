0405995c1f14dff4d65bfc36489ed71a688b37d1
Raul T <jr.torsa@gmail.com>
2020-09-07 14:47:52 -0600

    addin id to form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import uuid from "uuid/v4";
 
 const Formulario = () => {
   const [cita, actializarCita] = useState({
@@ -9,6 +10,8 @@ const Formulario = () => {
     sintomas: "",
   });
 
+  const [error, actualizarError] = useState(false);
+
   const actualizarState = (e) => {
     actializarCita({
       ...cita,
@@ -16,11 +19,44 @@ const Formulario = () => {
     });
   };
 
+  //extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  //agregar cita
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+    //eliminar el mensaje de error
+    actualizarError(false);
+
+    //asignar id
+    cita.id = uuid();
+    console.log(cita);
+    //crear cita
+
+    //reiniciar el form
+  };
+
   return (
     <>
       <h2>Crear Cita</h2>
 
-      <form>
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+
+      <form onSubmit={submitCita}>
         <label>Nombre Mascota</label>
         <input
           type="text"
@@ -28,6 +64,7 @@ const Formulario = () => {
           className="u-full-width"
           placeholder="Nombre Mascota"
           onChange={actualizarState}
+          value={mascota}
         />
         <label>Nombre Dueño</label>
         <input
@@ -36,6 +73,7 @@ const Formulario = () => {
           className="u-full-width"
           placeholder="Nombre Dueño de la Mascota"
           onChange={actualizarState}
+          value={propietario}
         />
         <label>Fecha</label>
         <input
@@ -43,6 +81,7 @@ const Formulario = () => {
           name="fecha"
           className="u-full-width"
           onChange={actualizarState}
+          value={fecha}
         />
         <label>Hora</label>
         <input
@@ -50,12 +89,14 @@ const Formulario = () => {
           name="hora"
           className="u-full-width"
           onChange={actualizarState}
+          value={hora}
         />
         <label>Sintomas</label>
         <textarea
           className="u-full-width"
           name="sintomas"
           onChange={actualizarState}
+          value={sintomas}
         ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Agregar Cita
